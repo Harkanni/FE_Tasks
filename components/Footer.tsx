cb56b5657cd7e41e21670f552f93ae298d0d060b
harkanni <harkanzyempire001@gmail.com>
2023-12-30 16:27:33 +0100

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import styles from '@/app/styles.module.css'
+import { FaGithub, FaLinkedin, FaTwitter, FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+   return (
+      <div className={styles.footer}>
+         <div className={styles.social}>
+            <FaGithub />
+            <FaLinkedin />
+            <FaTwitter />
+            <FaFacebook />
+         </div>
+      </div>
+   )
 }
 
 export default Footer
