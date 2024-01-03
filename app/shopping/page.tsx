c203b490df1e4327bdbbc4fd9be2512031a36a53
harkanni <harkanzyempire001@gmail.com>
2024-01-03 10:20:32 +0100

    extras

--- app/shopping/page.tsx
+++ app/shopping/page.tsx
@@ -9,6 +9,7 @@ import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 import { MdClose } from 'react-icons/md'
 import { IoSend } from "react-icons/io5";
 import { FaRedo } from "react-icons/fa";
+import { IoCheckmarkDoneSharp } from "react-icons/io5";
 
 // interface PersonProps {
 //    id: number,
@@ -32,21 +33,42 @@ const ShoppingList = () => {
    const [selectedPerson, setSelectedPerson] = useState('')
    const [query, setQuery] = useState('')
    const [clickedItems, setClickedItems] = useState<any>({})
+   const [isFetching, setIsFetching] = useState<boolean>(false)
 
    useEffect(() => {
+      console.log('called', query)
       const fetchData = async () => {
          try {
-            const response = await fetch(`https://api.frontendeval.com/fake/food/mi${query}`);
+            setIsFetching(true);
+
+            const abortController = new AbortController();
+            const signal = abortController.signal;
+
+            const response = await fetch(`https://api.frontendeval.com/fake/food/${query}`, { signal });
             const data = await response.json();
-            setAutocompleteList([...autocompleteList, ...data])
+
+            if (!signal.aborted) {
+               setAutocompleteList([...data]);
+            }
             console.log(data);
          } catch (error) {
             console.error('Error fetching data:', error);
+         } finally {
+            setIsFetching(false);
          }
       };
 
-      fetchData();
-   }, []);
+      const debouncedFetchData = () => {
+         clearTimeout(timeoutId);
+         if (query.length >= 2) {
+            timeoutId = setTimeout(fetchData, 500); // Adjust the debounce delay as needed
+         }
+      };
+
+      debouncedFetchData();
+
+      return () => clearTimeout(timeoutId);
+   }, [query]);
 
 
    const filteredPeople =
@@ -60,23 +82,6 @@ const ShoppingList = () => {
          )
 
    let timeoutId: any;
-
-   const fetchData = async () => {
-      try {
-         const response = await fetch(`https://api.frontendeval.com/fake/food/${query.toLowerCase()}`);
-         const data = await response.json();
-         setAutocompleteList([...data])
-         console.log(data);
-      } catch (error) {
-         console.error('Error fetching data:', error);
-      }
-   };
-
-   const debouncedFetchData = () => {
-      clearTimeout(timeoutId); // Clear any existing timeout
-      timeoutId = setTimeout(fetchData, 1000); // Set a new timeout for 5 seconds
-   };
-
 
    const handleStrikeClick = (itemId: any) => {
       // Toggle the clicked state for the item
@@ -93,9 +98,12 @@ const ShoppingList = () => {
    }
 
    const handleChange = (event: any) => {
-      // debouncedFetchData()
-      fetchData()
       setQuery(event.target.value)
+   }
+
+   const handleRemoveItem = (item: any) => {
+      const updatedPeople = people.filter((name: string) => name !== item);
+      setPeople(updatedPeople);
    }
 
    return (
@@ -106,7 +114,7 @@ const ShoppingList = () => {
                <div className="relative mt-1">
                   <div className="flex flex-row relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
                      <Combobox.Input
-                        className="capitalize flex-1 placeholder:font-light w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0 bg-slate-200"
+                        className="flex-1 placeholder:font-light w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0 bg-slate-200"
                         displayValue={(person: string) => person}
                         onChange={(event) => handleChange(event)}
                         placeholder='Add to shopping list'
@@ -122,59 +130,69 @@ const ShoppingList = () => {
                      leaveTo="opacity-0"
                      afterLeave={() => setQuery('')}
                   >
-                     <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-                        {filteredPeople.length === 0 && query !== '' ? (
-                           <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
-                              Nothing found.
-                           </div>
-                        ) : (
-                           filteredPeople.map((person: string, index: number) => (
-                              <Combobox.Option
-                                 key={index}
-                                 className={({ active }) =>
-                                    `capitalize relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-teal-600 text-white' : 'text-gray-900'
-                                    }`
-                                 }
-                                 value={person}
-                                 onClick={() => { setQuery(selectedPerson); console.log(selectedPerson) }} // ADDED
-                              >
-                                 {({ selected, active }) => (
-                                    <>
-                                       <span
-                                          className={`block truncate ${selected ? 'font-medium' : 'font-normal'
-                                             }`}
-                                       >
-                                          {person}
-                                       </span>
-                                       {selected ? (
+                     {isFetching
+                        ? <div> ...isLoading </div>
+                        : <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                           {filteredPeople.length === 0 && query !== '' ? (
+                              <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
+                                 Nothing found.
+                              </div>
+                           ) : (
+                              filteredPeople.map((person: string, index: number) => (
+                                 <Combobox.Option
+                                    key={index}
+                                    className={({ active }) =>
+                                       `capitalize relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-teal-600 text-white' : 'text-gray-900'
+                                       }`
+                                    }
+                                    value={person}
+                                    onClick={() => { setQuery(selectedPerson); console.log(selectedPerson) }} // ADDED
+                                 >
+                                    {({ selected, active }) => (
+                                       <>
                                           <span
-                                             className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? 'text-white' : 'text-teal-600'
+                                             className={`block truncate ${selected ? 'font-medium' : 'font-normal'
                                                 }`}
                                           >
-                                             <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                             {person}
                                           </span>
-                                       ) : null}
-                                    </>
-                                 )}
-                              </Combobox.Option>
-                           ))
-                        )}
-                     </Combobox.Options>
+                                          {selected ? (
+                                             <span
+                                                className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? 'text-white' : 'text-teal-600'
+                                                   }`}
+                                             >
+                                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                             </span>
+                                          ) : null}
+                                       </>
+                                    )}
+                                 </Combobox.Option>
+                              ))
+                           )}
+                        </Combobox.Options>
+                     }
                   </Transition>
                </div>
             </Combobox>
          </div>
 
-         <div className="mt-5 w-80  border-cyan-700 flex flex-col gap-2 border-spacing-90 overflow-hidden rounded-lg min-h-2">
+         <div className="mt-5 min-w-96  border-cyan-700 flex flex-col gap-2 border-spacing-90 overflow-hidden rounded-lg min-h-2">
             <h3>Shopping List</h3>
             {people.map((person: string, index: number) => (
-               <div key={index} className='bg-slate-200  flex flex-column justify-between'>
-                  <div className={`p-[.7rem] flex-1 text-gray-900 text-sm ${clickedItems[index] && styles.strikeThroughBG}`}>
-                     <p className={`capitalize ${clickedItems[index] && styles.strikeThrough}`}>{person}</p>
+               <div key={index} className='bg-slate-200  flex flex-column justify-between gap-2'>
+                  <div className={`p-[.7rem] flex-1 flex items-center gap-3 text-gray-900 text-sm ${clickedItems[index] && styles.strikeThroughBG}`}>
+                     <div className={`flex flex-[.2] cursor-pointer hover:opacity-90 overflow-hidden justify-center items-center`} onClick={() => handleStrikeClick(index)} >
+                        <IoCheckmarkDoneSharp size={24} />
+                     </div>
+                     <p className={`capitalize text-base ${clickedItems[index] && styles.strikeThrough}`}>{person}</p>
                   </div>
-                  <div className={`flex flex-[.2] cursor-pointer hover:opacity-90 bg-black ${clickedItems[index] && 'bg-[#424040fd]'} overflow-hidden justify-center items-center`} onClick={() => handleStrikeClick(index)} >
-                     {!clickedItems[index] ? <MdClose color='white' /> : <FaRedo color='white' />}
-
+                  <div className='flex gap-2'>
+                     <p className='text-black p-2'>10</p>
+                     <button className='px-1.5 text-black border border-slate-700 rounded-full'>+</button>
+                     <button className='px-2 text-black border border-slate-700 rounded-full'>-</button>
+                  </div>
+                  <div className={`flex flex-[.2] cursor-pointer hover:opacity-90 bg-black ${clickedItems[index] && 'bg-[#424040fd]'} overflow-hidden justify-center items-center`} onClick={() => handleRemoveItem(person)} >
+                     { !clickedItems[index] ? <MdClose color='white' /> : null }
                   </div>
                </div>
             ))}
