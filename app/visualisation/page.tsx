9f62da314eadbd9c01f9eaf5eb5a7298a13d6be4
harkanni <harkanzyempire001@gmail.com>
2024-01-10 14:05:02 +0100

    barchat

--- app/visualisation/page.tsx
+++ app/visualisation/page.tsx
@@ -1,18 +1,48 @@
-import React from 'react'
+"use client"
+
+import React, { useState, useContext } from 'react'
 import styles from '@/app/styles.module.css'
 
 import { Bar } from 'react-chartjs-2'
 
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from 'chart.js'
+import { ThemeManagementContext } from '@/components/MainWrapper'
 
 
 ChartJS.register(
    CategoryScale, LinearScale, BarElement, Tooltip, Legend
 )
 
+const users = [
+   { name: "John", items_delivered: 22, miles_driven: 100 },
+   { name: "Jane", items_delivered: 96, miles_driven: 164 },
+   { name: "Mary", items_delivered: 40, miles_driven: 106 },
+   { name: "Bob", items_delivered: 122, miles_driven: 172 },
+   { name: "Alice", items_delivered: 43, miles_driven: 122 },
+   { name: "Joe", items_delivered: 74, miles_driven: 110 },
+   { name: "Sue", items_delivered: 93, miles_driven: 149 },
+]
+
 const Visualization = () => {
+   const [isDark, setTheme ] = useContext(ThemeManagementContext)
+   console.log(isDark)
+   
    return (
-      <div>Visualization</div>
+      <div>
+         <Bar
+            data={{
+               labels: users.map(user => user.name),
+               datasets: [
+                  {
+                     label: "Items delivered",
+                     data: users.map(user => user.items_delivered),
+                     backgroundColor: "grey",
+                     borderRadius: 3
+                  }
+               ]
+            }}
+         />
+      </div>
    )
 }
 
--- components/MainWrapper.tsx
+++ components/MainWrapper.tsx
@@ -1,25 +1,31 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useState, createContext } from 'react'
 import { Theme } from '@radix-ui/themes';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import SidebarNavigation from './SidebarNavigation';
 
+interface ThemeManagementContextType { }
+
+export const ThemeManagementContext = createContext<ThemeManagementContextType | any>(undefined);
+
 const MainWrapper = ({ children }: { children: React.ReactNode }) => {
    const [isDark, setTheme] = useState<boolean>(true);
    const [sidebarActive, setSidebarActive] = useState(false);
    return (
-      <Theme appearance={isDark ? 'dark' : 'light'} style={{ minHeight: '84vh' }}>
-         <Navbar isDark={isDark} setTheme={setTheme} sidebar={sidebarActive} setSidebar={setSidebarActive} />
-         {sidebarActive && <SidebarNavigation sidebar={sidebarActive} setSidebar={setSidebarActive} />}
+      <ThemeManagementContext.Provider value={[isDark, setTheme]}>
+
+         <Theme appearance={isDark ? 'dark' : 'light'} style={{ minHeight: '84vh' }}>
+            <Navbar isDark={isDark} setTheme={setTheme} sidebar={sidebarActive} setSidebar={setSidebarActive} />
+            {sidebarActive && <SidebarNavigation sidebar={sidebarActive} setSidebar={setSidebarActive} />}
 
 
 
-         {children}
+            {children}
 
-
-      </Theme>
+         </Theme>
+      </ThemeManagementContext.Provider>
    )
 }
 
