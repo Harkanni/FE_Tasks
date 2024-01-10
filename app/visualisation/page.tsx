a6ba30a7f57b2b55993535638d5ba8abc18a2aa9
harkanni <harkanzyempire001@gmail.com>
2024-01-10 13:20:20 +0100

    barchat

--- app/visualisation/page.tsx
+++ app/visualisation/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import styles from '@/app/styles.module.css'
+
+import { Bar } from 'react-chartjs-2'
+
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from 'chart.js'
+
+
+ChartJS.register(
+   CategoryScale, LinearScale, BarElement, Tooltip, Legend
+)
 
 const Visualization = () => {
-  return (
-    <div>Visualization</div>
-  )
+   return (
+      <div>Visualization</div>
+   )
 }
 
 export default Visualization
