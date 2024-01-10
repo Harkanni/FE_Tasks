42e60c0dc74b895a1c599be81ee1fd6efa535058
harkanni <harkanzyempire001@gmail.com>
2024-01-10 15:27:46 +0100

    barchat

--- app/visualisation/page.tsx
+++ app/visualisation/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import styles from '@/app/styles.module.css'
 
-import { Bar } from 'react-chartjs-2'
+import { Bar, } from 'react-chartjs-2'
 
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from 'chart.js'
 import { ThemeManagementContext } from '@/components/MainWrapper'
@@ -24,25 +24,65 @@ const users = [
 ]
 
 const Visualization = () => {
-   const [isDark, setTheme ] = useContext(ThemeManagementContext)
-   console.log(isDark)
-   
+   const [isDark, setTheme] = useContext(ThemeManagementContext)
+   const [randomNumber, setRandomNumber] = useState<any>()
+
+   useEffect(() => {
+      const fetchRandomNumbers = async () => {
+         try {
+            const response = await fetch(
+               'https://www.random.org/integers/?num=200&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+            );
+            const data = await response.text();
+            const numbers: number[] = data.trim().split('\n').map(Number);
+
+            const frequencyMap: Record<number, number> = numbers.reduce((map: any, number: any) => {
+               map[number] = (map[number] || 0) + 1;
+               return map;
+            }, {});
+
+            const bar_data: { name: string; frequency_count: number }[] = Object.entries(frequencyMap).map(
+               ([name, frequency_count]) => ({
+                  name,
+                  frequency_count,
+               })
+            );
+
+            setRandomNumber(bar_data);
+            console.log(bar_data);
+         } catch (error) {
+            console.error('Error fetching random numbers:', error);
+         }
+      };
+
+
+
+      fetchRandomNumbers()
+   }, [])
+
    return (
-      <div>
-         <Bar
-            data={{
-               labels: users.map(user => user.name),
-               datasets: [
-                  {
-                     label: "Items delivered",
-                     data: users.map(user => user.items_delivered),
-                     backgroundColor: "grey",
-                     borderRadius: 3
-                  }
-               ]
-            }}
-         />
-      </div>
+      <section className={styles.visualisationContainer}>
+         <div className={styles.chartBox}>
+            <Bar
+               options={{ responsive: true, }}
+               data={{
+                  labels: randomNumber?.map((user: any) => user.name),
+                  datasets: [
+                     {
+                        label: "frequency_count",
+                        data: randomNumber?.map((user: any) => user.frequency_count),
+                        backgroundColor: "grey",
+                        borderRadius: 3,
+                        hoverBackgroundColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
+                     }
+                  ],
+
+               }}
+
+            />
+         </div>
+
+      </section>
    )
 }
 
